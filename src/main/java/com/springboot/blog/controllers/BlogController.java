27a8b11f16855b91854ac0611a6dcaea7dc8277a
Janara3 <tashievajanara123@gmail.com>
2023-03-29 13:23:37 +0600

    Initial commit

---
+++ src/main/java/com/springboot/blog/controllers/BlogController.java
@@ -0,0 +1,17 @@
+package com.springboot.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController<Mobel> {
+    @GetMapping("/blog")
+    public String blogMain() {
+        return blogMain();
+    }
+
+    @GetMapping("/blog")
+    public String blogMain(Mobel mobel){
+        return "blog-main";
+    }
+}
